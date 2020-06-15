062ee25aa0ab16518ec7e66664b582f903b8d99c
bonatgy <1300151235@qq.com>
2020-06-15 22:56:32 +0800

    HeartBeatServer

---
+++ src/main/java/com/xz/netty/heartBeat/HeartBeatClient.java
@@ -0,0 +1,54 @@
+package com.xz.netty.heartBeat;
+
+import com.xz.netty.groupChat.basic.GroupChatClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.Scanner;
+
+public class HeartBeatClient {
+    private String host;
+    private int port;
+
+    public HeartBeatClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            pipeline.addLast("decoder", new StringDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            socketChannel.pipeline().addLast(new GroupChatClientHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            Channel channel = channelFuture.channel();
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                channel.writeAndFlush(s + "\r\n");
+            }
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new HeartBeatClient("127.0.0.1", 7777).run();
+    }
+}
---
+++ src/main/java/com/xz/netty/heartBeat/HeartBeatServer.java
@@ -0,0 +1,41 @@
+package com.xz.netty.heartBeat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeartBeatServer {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new IdleStateHandler(2, 3, 6, TimeUnit.SECONDS));
+                            pipeline.addLast(new HeartBeatServerHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind(7777).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
