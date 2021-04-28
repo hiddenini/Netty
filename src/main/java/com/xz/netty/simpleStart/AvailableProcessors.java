ee5f9f05667bfe4a1c2608df1e0207aecb101f07
bonatgy <1300151235@qq.com>
2021-04-28 20:29:46 +0800

    netty note

--- src/main/java/com/xz/netty/simpleStart/AvailableProcessors.java
+++
@@ -1,9 +0,0 @@
-package com.xz.netty.simpleStart;
-
-import io.netty.util.NettyRuntime;
-
-public class AvailableProcessors {
-    public static void main(String[] args) {
-        System.out.println(NettyRuntime.availableProcessors());
-    }
-}
